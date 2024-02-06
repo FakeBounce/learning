2bf58e25a02493d9dd00e2945e89e9576e206072
Benjo <benjamin@market-academy.com>
2024-02-06 10:58:33 +0100

    Added button component to LMS specific design

--- src/pages/login/LoginFooter.tsx
+++ src/pages/login/LoginFooter.tsx
@@ -1,28 +1,12 @@
-// @mui
 import { Trans } from '@lingui/macro';
-import { LoadingButton } from '@mui/lab';
-
-import theme from '@theme';
+import ActionButton from '@src/components/lms/ActionButton';
 
 // ----------------------------------------------------------------------
 
 export default function LoginFooter({ isLoading }: { isLoading: boolean }) {
   return (
-    <LoadingButton
-      fullWidth
-      size="large"
-      type="submit"
-      loading={isLoading}
-      sx={{
-        borderRadius: 2,
-        bgcolor: theme.palette.green[700],
-        color: 'white',
-        '&:hover': {
-          bgcolor: theme.palette.green[800]
-        }
-      }}
-    >
+    <ActionButton fullWidth size="large" type="submit" loading={isLoading}>
       <Trans>Se connecter</Trans>
-    </LoadingButton>
+    </ActionButton>
   );
 }
