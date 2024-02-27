056e2dd15cb5391420aa3afa44f2ed03a7564873
Tom Carrillo <tom@market-academy.com>
2024-02-27 14:49:31 +0100

    Make a call to get current user profile infos :
    https://app.asana.com/0/1206016644344592/1206185791384337

--- src/pages/users/user-profile/UserProfile.tsx
+++ src/pages/users/user-profile/UserProfile.tsx
@@ -1,14 +1,37 @@
 import { Box } from '@mui/material';
 import { LMSCard } from '@src/components/lms';
-import UserProfileInfos from '@src/pages/users/user-profile/UserProfileInfos.tsx';
-import UserProfileHeader from '@src/pages/users/user-profile/UserProfileHeader.tsx';
-import UserProfileGroups from '@src/pages/users/user-profile/user-groups/UserProfileGroups.tsx';
+import UserProfileInfos from '@src/pages/users/user-profile/UserProfileInfos';
+import UserProfileHeader from '@src/pages/users/user-profile/UserProfileHeader';
+import UserProfileGroups from '@src/pages/users/user-profile/user-groups/UserProfileGroups';
+import UserProfileRoles from '@src/pages/users/user-profile/user-roles/UserProfileRoles';
+import { useAppDispatch, useAppSelector } from '@redux/hooks';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import UserProfileRoles from '@src/pages/users/user-profile/user-roles/UserProfileRoles.tsx';
+import { getSingleUser } from '@redux/reducers/usersReducer';
+import { User } from '@services/connected-user/interfaces.ts';
 
 export default function UserProfile() {
-  const { state } = useLocation();
-  const { user } = state;
+  const dispatch = useAppDispatch();
+  const [user, setUser] = useState<User>({} as User);
+
+  const { currentUserData } = useAppSelector(
+    (state) => state.users.currentUser
+  );
+  const { pathname } = useLocation();
+  const userId = Number(pathname.split('/').pop());
+
+  useEffect(() => {
+    console.log("je passe ici");
+    if(userId) {
+      dispatch(getSingleUser(userId));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (currentUserData) {
+      setUser(currentUserData);
+    }
+  }, [currentUserData]);
 
   return (
     <Box px={[0, 2]} display='flex' width="100%">
@@ -17,9 +40,9 @@ export default function UserProfile() {
 
         <UserProfileInfos user={user} />
 
-        <UserProfileGroups groups={user.groups} />
+        <UserProfileGroups groups={user.groups ?? []} />
 
-        <UserProfileRoles roles={user.roles} />
+        <UserProfileRoles roles={user.roles ?? []} />
       </LMSCard>
     </Box>
   );
