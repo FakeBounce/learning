628026d208bfc71541b944ce2e2c4dfc459adb11
Tom Carrillo <tom@market-academy.com>
2024-02-28 14:05:29 +0100

    Create LMSPopover reusable component + Add logout button with it
    https://app.asana.com/0/1206016644344592/1206185791583903

--- src/components/layouts/sidebar/SidebarUser.tsx
+++ src/components/layouts/sidebar/SidebarUser.tsx
@@ -1,9 +1,25 @@
-import { Box, Stack, Avatar } from '@mui/material';
+import { Box, Stack, Avatar, ListItemButton, ListItemText, ListItem } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import LMSPopover from '@src/components/lms/LMSPopover';
+import { Trans } from '@lingui/macro';
+import { MouseEvent, useState } from 'react';
+import { useAuthenticationContext } from '@src/auth/AuthenticationContext';
 // ----------------------------------------------------------------------
 
 export default function SidebarUser({ open }: { open: boolean }) {
   const theme = useTheme();
+  const { logout } = useAuthenticationContext();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = () => (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => setAnchorEl(null);
+
+  const popoverOpen = Boolean(anchorEl);
+  const id = popoverOpen ? 'logout-popover' : undefined;
+
   if (!open) {
     return (
       <Stack
@@ -41,8 +57,10 @@ export default function SidebarUser({ open }: { open: boolean }) {
           borderRadius: 2,
           height: '5rem',
           display: 'flex',
-          transition: 'background-color 0.5s ease'
+          transition: 'background-color 0.5s ease',
+          cursor: 'pointer'
         }}
+        onClick={handleClick()}
       >
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', ml: 2, mr: 2 }}>
           <Avatar alt="Avatar photo" src="/assets/shape_avatar.svg" />
@@ -50,6 +68,23 @@ export default function SidebarUser({ open }: { open: boolean }) {
         <Stack py={2} spacing={1}>
           <Box>Nom / Prénom</Box> <Box> Rôle</Box>
         </Stack>
+        <LMSPopover
+          id={id}
+          anchorEl={anchorEl}
+          popoverOpen={popoverOpen}
+          onClose={handleClose}
+        >
+          <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              onClick={() => {
+                handleClose();
+                logout();
+              }}
+            >
+              <ListItemText primary={<Trans>Déconnexion</Trans>} />
+            </ListItemButton>
+          </ListItem>
+        </LMSPopover>
       </Box>
     </Stack>
   );
---
+++ src/components/lms/LMSPopover.tsx
@@ -0,0 +1,41 @@
+import React, { } from 'react';
+import { ClickAwayListener, Paper, Popover } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+interface LMSPopoverProps {
+  id?: string;
+  popoverOpen: boolean;
+  onClose: () => void;
+  anchorEl: HTMLElement | null;
+  children: React.ReactNode;
+}
+export default function LMSPopover(
+  { id, popoverOpen, onClose, anchorEl, children }: LMSPopoverProps
+) {
+  const theme = useTheme();
+
+  return (
+    <Popover
+      id={id}
+      open={popoverOpen}
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 15,
+        horizontal: 'center',
+      }}
+    >
+      <ClickAwayListener onClickAway={onClose}>
+        <Paper
+          elevation={10}
+          sx={{
+            borderRadius: 2,
+            minWidth: 160,
+            '&:hover': { color: theme.palette.secondary.main },
+          }}
+        >
+          {children}
+        </Paper>
+      </ClickAwayListener>
+    </Popover>
+  )
+}
