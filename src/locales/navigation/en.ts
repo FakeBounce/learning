506a4864889c6560a213c60e1eb2ac0fe4102c62
Benjo <benjamin@market-academy.com>
2024-03-11 10:23:52 +0100

    Clean permissions getter and connectedUser state

--- src/locales/navigation/en.ts
+++
@@ -1,3 +0,0 @@
-/*eslint-disable*/ export const messages = JSON.parse(
-  '{"2mcnZQ":"Utilisateurs","3RK7em":"Liste des modules","7p5kLi":"Dashboard","9wYFQH":"Groupes","EIxP9b":"Gestion des rôles","GoZkew":"organizations","HbvbX+":"Personnalisation","Xukbwg":"Modifier une organization","eoO6cw":"Liste des Parcours","hsf9ph":"Étudiants","q+G9Ft":"Créer une organization","v+QFYv":"Testeurs externes"}'
-);
