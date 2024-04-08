38e7a649ce6fd7aa619b0a4e8bbd909058c1231e
Benjo <benjamin@market-academy.com>
2024-04-08 08:42:16 +0200

    Updated feature flag  with new array from api

--- src/utils/feature-flag/FeatureFlagProvider.tsx
+++ src/utils/feature-flag/FeatureFlagProvider.tsx
@@ -1,8 +1,9 @@
 import { ReactNode, useState, createContext, useEffect, useCallback } from 'react';
 import {
-  PermissionAction,
   PermissionEnum,
-  PermissionTypeEnum
+  PermissionKey,
+  PermissionTypeEnum,
+  UserPermission
 } from '@services/permissions/interfaces';
 import { useAppSelector } from '@redux/hooks';
 
@@ -11,6 +12,21 @@ enum HasAdminRights {
   CLIENT_ADMIN = 1,
   SUPER_ADMIN = 2
 }
+
+type UserPermissionObject = {
+  [key in PermissionKey]: boolean;
+};
+
+const constructPermissionsObject = () => {
+  const permissionsObject: UserPermissionObject = {} as UserPermissionObject;
+  Object.values(PermissionTypeEnum).forEach((typeEnum) => {
+    Object.values(PermissionEnum).forEach((permEnum) => {
+      const key = `${permEnum}_${typeEnum}`;
+      permissionsObject[key as PermissionKey] = false;
+    });
+  });
+  return permissionsObject;
+};
 
 export const FeatureFlagContext = createContext<{
   isAuthorizedByPermissionsTo: (
@@ -25,6 +41,9 @@ export const FeatureFlagContext = createContext<{
 
 const FeatureFlagProvider = ({ children }: { children: ReactNode }) => {
   const [isUserAdmin, setIsUserAdmin] = useState<HasAdminRights>(HasAdminRights.NO);
+  const [permissionsObject, setPermissionsObject] = useState<UserPermissionObject>(
+    constructPermissionsObject()
+  );
   const { isSuperAdmin, isClientAdmin, permissions, currentOrganization } = useAppSelector(
     (state) => state.connectedUser.user
   );
@@ -37,28 +56,23 @@ const FeatureFlagProvider = ({ children }: { children: ReactNode }) => {
     }
   }, [isSuperAdmin, isClientAdmin]);
 
+  useEffect(() => {
+    if (permissions && permissions.length > 0) {
+      const newPermissionsObject = constructPermissionsObject();
+      permissions.forEach((permission: UserPermission) => {
+        newPermissionsObject[permission.name] = true;
+      });
+      setPermissionsObject(newPermissionsObject);
+    }
+  }, [permissions]);
+
   const isAuthorizedByPermissionsTo = useCallback(
     (pageType: PermissionTypeEnum, permissionAsked: PermissionEnum): boolean => {
       if (isUserAdmin) {
         return true;
       }
-      if (permissions[pageType] && permissions[pageType].actions) {
-        // If the permission asked is READ, we check if the permission exists in the array
-        if (permissionAsked === PermissionEnum.READ) {
-          const hasKeyIndex = permissions[pageType].actions.findIndex(
-            (action: PermissionAction) => action.name === PermissionEnum.READ
-          );
-          if (hasKeyIndex !== -1) {
-            return permissions[pageType].actions[hasKeyIndex].enabled;
-          }
-          // If the permission doesn't exist, that means everyone can visualize it
-          return true;
-        }
-        return permissions[pageType].actions.some(
-          (action: PermissionAction) => action.name === permissionAsked && action.enabled
-        );
-      }
-      return false;
+
+      return permissionsObject[`${permissionAsked}_${pageType}`];
     },
     [permissions, isUserAdmin]
   );
